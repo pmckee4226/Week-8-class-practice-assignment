e9cf4b6f9c53b7724198e88b758a124f41826a40
pmckee4226 <patrickmckee52@gmail.com>
2022-05-26 23:28:14 -0700

    Add project files.

---
+++ Week 8 class practice assignment/Methods.cs
@@ -0,0 +1,38 @@
+﻿//Patrick McKee
+//May 26, 2022
+//Classes Practice Assignment
+
+// See https://aka.ms/new-console-template for more information
+using Week_8_class_practice_assignment;
+
+Console.WriteLine("Hello, Professor Huitsing!");
+
+Person person = new Person();
+person.FirstName = "Patrick";
+person.LastName = "McKee";
+person.Email = "patrickmckee52@gmail.com";
+person.Job = "Executive Customer Relations Specialist at Amazon Web Services.";
+person.DisplayName();
+person.DisplayEmail();
+person.DisplayJob();
+
+var person2 = new Person();
+var person3 = new Person();
+
+person2.FirstName = "Eric";
+person2.LastName = "Gintz";
+person2.Email = "idk101@madeupemail.net";
+person2.Job = "Private Pricing Contracts Specialist at Amazon Web Services.";
+person2.DisplayName();
+person2.DisplayEmail();
+person2.DisplayJob();
+
+person3.FirstName = "Renly";
+person3.LastName = "Barkratheon";
+person3.Email = "spoiled_husky_puppy@madeupemail.net";
+person3.Job = "The Reason My Dads Keep Their Jobs at home";
+person3.DisplayName();
+person3.DisplayEmail();
+person3.DisplayJob();
+
+Console.WriteLine("");
---
+++ Week 8 class practice assignment/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Patrick McKee
+//May 26, 2022
+//Classes Practice Assignment
+
+namespace Week_8_class_practice_assignment
+{
+    public class Person
+    {
+        //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Job { get; set; }
+
+        //methods (behavior)
+        public void DisplayName()
+        {
+            Console.WriteLine("Hello, my name is {0} {1}",FirstName, LastName);
+
+        }
+        public void DisplayEmail()
+        {
+            Console.WriteLine("My email address is " + Email);
+
+        }
+        public void DisplayJob()
+        {
+            Console.WriteLine("My job title is {0}", Job);
+        }
+    }
+}
